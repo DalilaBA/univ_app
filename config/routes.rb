a59910b7e4ecd627c340cd48781b3156e23e4518
Dalila <babaameur.dalila@gmail.com>
2021-09-14 13:07:59 +0100

    added validations for corses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :courses
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'courses/index', to:'courses#index'
   get 'courses/new', to: 'courses#new'
 end
