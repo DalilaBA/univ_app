503b60712afe479f640c46a88679f83750979577
Dalila <babaameur.dalila@gmail.com>
2021-09-14 19:05:51 +0100

    styling students index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validate :name, presence: true, length: minimum: 4, maximum: 10}
+  validates :name, presence: true, length: {minimum: 3, maximum: 10}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                             
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false}, 
