0da910bae0bdbb610c51c64c699b48dd986682d5
Dalila <babaameur.dalila@gmail.com>
2021-09-19 21:37:34 +0100

    create many to many association

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: {minimum: 3, maximum: 15}
   validates :description, presence: true, length: {minimum: 5, maximum: 50}
 
+  has_many :user_course
+  has_many :user, through: :user_course
 end
